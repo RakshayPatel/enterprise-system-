4f53658063fd53386f9d5201d636f699f6b1c376
Rakshay <rakshay37@gmail.com>
2024-05-26 15:43:35 -0400

    Basic V0

---
+++ profile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def get_user_info():
+    name = input("What is your name? ")
+    
+    age = input("How old are you? ")
+    
+    study = input("What are you studying? ")
+    
+    print("\n--- User Information ---")
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Studying: {study}")
+
+get_user_info()
+
+
+# In[ ]:
+
+
+
+
